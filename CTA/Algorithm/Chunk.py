a1fac9b97358b479f886a9b8ce414d291d05177e
Hadar <hadar.rahamim91@gmail.com>
2019-04-12 15:37:29 +0300

    Create word2vec wrapper module

--- CTA/Algorithm/Chunk.py
+++ CTA/Algorithm/Chunk.py
@@ -1,16 +1,18 @@
 from Utils import logger
 from Utils import configuration
+from Utils import Word2VecWrapper
 import logging
 
 
 class Chunk(object):
 
     def __init__(self, config):
-        self.chunk_size = config.get("CHUNKS", "chunk_size")
+        self.chunk_size = config.get("CHUNKS", "size")
         self.delay = config.get("CHUNKS", "delay")
 
         self.log = logging.getLogger(__name__ + "." + __class__.__name__)
-        self.log = logger.add_log_file(self.log, config)
+        self.log = logger.setup()
+        #self.log = logger.add_log_file(self.log, config)
 
 
     def print_delay(self):
@@ -25,3 +27,26 @@ if __name__ == "__main__":
     config = configuration.config().setup()
     chunk2 = Chunk(config)
     # functions
+
+    # option 1: external word embedding
+    model = Word2VecWrapper.Model(config, filepath="C:\\Users\\Hadar\\Downloads\\wiki.he\\wiki.he.vec")
+
+    # option 2: create word embedding based on the documents collection
+    #documents = ["C:\\Users\\Hadar\\Downloads\\texts\\Bamidbar_chapter_B.txt",
+    #             "C:\\Users\\Hadar\\Downloads\\texts\\Bereshit_chapter_A.txt",
+    #             "C:\\Users\\Hadar\\Downloads\\texts\\MelachimB_chapter_C.txt",
+    #             "C:\\Users\\Hadar\\Downloads\\texts\\Shoftim_chapter_H.txt",
+    #             "C:\\Users\\Hadar\\Downloads\\texts\\Yiov_chapter_A.txt"]
+#
+    #model = Word2VecWrapper.Model(config, documents=documents)
+
+
+    model.build_model()
+
+    word = "אהבה"
+    if (model.exist_in_vocab(word)):
+        print(str(model.get_vector(word)))
+
+    word = "אֶ֖לֶף"
+    if (model.exist_in_vocab(word)):
+        print(str(model.get_vector(word)))
--- CTA/Utils/Word2VecWrapper.py
+++ CTA/Utils/Word2VecWrapper.py
@@ -1,34 +1,112 @@
 from gensim.models import word2vec
 from gensim.models import KeyedVectors
 from gensim.test.utils import datapath
-
+from Utils import logger
+from Utils import configuration
+import logging
+import re
 
 class Model(object):
+    """
+    This class supply a wrapper for word embedding technique based on word2vec module.
+    Either instance of the class could be based on external word2vec file:
+        model = Model(config, filepath=[*.vec])
 
-    def __init__(self, config, filepath=None, documents=None):
+    Or creation of new vocabulary based on documents collection:
+        documents_collection = []
+        model = Model(config, documents=documents_collection)
+
+    Both ways should call to build_model function in order to finish the model setup.
+    """
+    def __init__(self, config, filepath=None, documents=[]):
         self.architecture = 1 if (config.get("Word2Vec", "arch") == "Skip-Gram") else 0
         self.training_model = 1 if (config.get("Word2Vec", "training_model") == "Hierarchical Softmax") else 0
-        self.window = config.get("Word2Vec", "context_window")
-        self.dimension = config.get("Word2Vec", "dimension")
+        self.window = int(config.get("Word2Vec", "context_window"))
+        self.dimension = int(config.get("Word2Vec", "dimension"))
+        self.delimiter = config.get("GENERAL", "text_delimiter")
         self.file_path = filepath
-        self.sentenaces = []
+        self.sentences = []
         self.vectors = None
+        self.log = logging.getLogger(__name__ + "." + __class__.__name__)
+        self.log = logger.setup()
+        #self.log = logger.add_log_file(self.log, config)
         if (documents):
-            self.set_sentenaces(documents)
+            self.set_sentences(documents)
 
     def build_model(self):
+        """
+        Build the word2vec model.
+        Either with external word embedding or based on document collection.
+        :return:
+        """
+        self.log.info("Build Model")
         if (self.file_path):
-            self.word_vectors = KeyedVectors.load_word2vec_format(datapath(self.file_path), binary=True)
+            self.word_vectors = KeyedVectors.load_word2vec_format(datapath(self.file_path), limit=50000)
         else:
-            self.model = word2vec(sentences=self.sentences, size=self.dimension, sg=self.architecture,
-                                  hs=self.training_model, window=self.window)
+            self.model = word2vec.Word2Vec(sentences=self.sentences, size=self.dimension, sg=self.architecture,
+                                  hs=self.training_model, window=self.window, min_count=1)
+            self.log.info(self.model)
             self.model.train(self.sentences, total_examples=self.model.corpus_count, epochs=self.model.iter)
             self.word_vectors = self.model.wv
 
-    def set_sentenaces(self, documents):
+    def set_sentences(self, documents):
+        """
+        This function parse and build list of list which represent words in sentences in the entire document collection.
+        example:
+            [['You', 'love', 'me'], ['The', 'dog', 'looks', 'nice']]
+
+        :param documents: list of paths to the document collection
+        :return:
+        """
+        self.log.info("Set Sentences")
         for doc in documents:
-            doc_sentences = word2vec.LineSentence(datapath(doc))
-            self.sentences.extend(doc_sentences)
+            with open(doc, mode='r', encoding='utf-8', errors='ignore') as file_handler:
+                text = file_handler.read()
+                text = re.sub(r'[,:;.]', '', text)                              # remove special chars
+                text = re.sub(r'\s.?\s', '', text)                              # remove words with length 1
+                sen_list = re.split(self.delimiter, text)                       # get sentences by delimiter
+                word_sen_list = [lst.strip().split(' ') for lst in sen_list]    # separate words in sentences
+                self.sentences.extend(sen for sen in word_sen_list)
+
 
     def get_vector(self, word):
-        return self.word_vectors[word]
+        """
+        Get the vector representation of the word.
+        :param word: word string
+        :return: if exist -> word vector, otherwise -1
+        """
+        self.log.info("Get Vector: " + word)
+        try:
+            vec = self.word_vectors[word]
+        except Exception as e:
+            self.log.error(str(e))
+            vec = -1
+        return vec
+
+    def exist_in_vocab(self, word):
+        return (word in self.word_vectors)
+
+if __name__ == "__main__":
+    config = configuration.config().setup()
+
+    # option 1: external word embedding
+    model = Model(config, filepath="C:\\Users\\Hadar\\Downloads\\wiki.he\\wiki.he.vec")
+
+    # option 2: create word embedding based on the documents collection
+    #documents = ["C:\\Users\\Hadar\\Downloads\\texts\\Bamidbar_chapter_B.txt",
+    #             "C:\\Users\\Hadar\\Downloads\\texts\\Bereshit_chapter_A.txt",
+    #             "C:\\Users\\Hadar\\Downloads\\texts\\MelachimB_chapter_C.txt",
+    #             "C:\\Users\\Hadar\\Downloads\\texts\\Shoftim_chapter_H.txt",
+    #             "C:\\Users\\Hadar\\Downloads\\texts\\Yiov_chapter_A.txt"]
+#
+    #model = Model(config, documents=documents)
+    model.build_model()
+
+    word = "אֶ֖לֶף"
+    if(model.exist_in_vocab(word)):
+        print(str(model.get_vector(word)))
+
+    word = "אהבה"
+    if (model.exist_in_vocab(word)):
+        print(str(model.get_vector(word)))
+
