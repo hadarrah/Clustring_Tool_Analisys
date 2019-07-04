Change spearman to chebyshev distance + fix silhouette calculation

CTA/Algorithm/chunk.py
@@ -1,7 +1,7 @@
 from Utils import logger
 import numpy as np
 import logging
-
+import math
 
 class Chunk(object):
 
@@ -17,6 +17,7 @@ class Chunk(object):
         self.model = model
         self.cluster = None
         self.preChunks = preChunks
+        self.ranked_vec = {}
         Chunk.createVec(self)
 
     def get_precursors_chunks(self):
@@ -77,3 +78,17 @@ class Chunk(object):
                 cosResult.append(cos)
             i += 1
         self.chunkVec = np.array(cosResult)
+        self.create_cumulative_vector(self.chunkVec)
+
+    def create_cumulative_vector(self, vector):
+        self.cummulative_vector = np.cumsum(vector)
+        size = len(self.cummulative_vector)
+        interval = math.floor(size/20)
+        vec = []
+        for i in range(0, size, interval):
+            vec.append(self.cummulative_vector[i])
+
+        self.shrinked_vector = np.array(vec)
+
+    def get_shrinked_cummulative_vec(self):
+        return self.shrinked_vector

CTA/Algorithm/distance_matric.py
@@ -1,9 +1,8 @@
 from Utils import logger
 import numpy as np
 import pandas as pd
-import math
 import logging
-
+from sklearn.neighbors import DistanceMetric as DM
 
 class Distance_Matric(object):
     """
@@ -11,7 +10,7 @@ class Distance_Matric(object):
     The input is a documents collection with chunks.
     """
     vectors_dict = {}
-    corr_matrix = None
+    chebyshev_distance = None
 
     def __init__(self, config, documents):
         self.config = config
@@ -20,6 +19,7 @@ class Distance_Matric(object):
         self.comparable_chunks = []
         self.chunks_index = {}
         self.transferred_vectors = []
+        self.chebyshev_mat = []
         Distance_Matric.vectors_dict = {}
         Distance_Matric.corr_matrix = None
 
@@ -34,24 +34,17 @@ class Distance_Matric(object):
         np.seterr(divide='ignore', invalid='ignore')
 
         # get all the comparable chunks
-        vector_size = int(self.config.get("CHUNKS", "size"))
-        vector_size = int((vector_size*(vector_size-1))/2)
         i = 0
         for doc in self.documents:
             self.comparable_chunks.extend(doc.get_comparable_chunks())
             for chunk in doc.get_chunks():
-                # transfer chunks vector and treat them as a columns in order to use pd.DataFrame.corr function
-                for j in range(0, vector_size):
-                    if (i == 0):
-                        initial_vector = [chunk.getchunkVec()[j]]
-                        self.transferred_vectors.append(initial_vector)
-                    else:
-                        self.transferred_vectors[j].append(chunk.getchunkVec()[j])
+                self.chebyshev_mat.append(chunk.get_shrinked_cummulative_vec())
                 Distance_Matric.vectors_dict["{}{}".format(chunk.getdocID(), chunk.getChunkID())] = i
                 i += 1
 
         self.log.info("total comparable chunks: " + str(len(self.comparable_chunks)))
-        self.create_spearman_matrix()
+
+        self.create_chebyshev_matrix()
 
         # iterate all over the comparable chunks and build their row in the metric
         i = 0
@@ -75,6 +68,7 @@ class Distance_Matric(object):
         self.distance_metric = np.matrix(self.distance_metric)
         np.set_printoptions(precision=3)
         self.log.info("Distance Matrix:")
+        self.log.info(self.distance_metric)
         for row in self.distance_metric:
             self.log.info(row)
 
@@ -92,15 +86,15 @@ class Distance_Matric(object):
 
         return abs(zv_homogeneous_1 + zv_homogeneous_2 - zv_heterogeneous_1 - zv_heterogeneous_2)
 
-    def create_spearman_matrix(self):
+    def create_chebyshev_matrix(self):
         """
-        Create spearman correlation matrix for all chunks (including the incomparable chunks)
+        Create Chebyshev distance matrix for all chunks (including the incomparable chunks)
         :return:
         """
-        self.log.info("Calculate spearman correlation matrix")
-        df = pd.DataFrame(self.transferred_vectors)
-        Distance_Matric.corr_matrix = df.corr(method="spearman")
-        print(str(Distance_Matric.corr_matrix))
+        dist = DM.get_metric('chebyshev')
+        Distance_Matric.chebyshev_distance = dist.pairwise(self.chebyshev_mat)
+        self.log.info(str(Distance_Matric.chebyshev_distance))
+
 
     @staticmethod
     def compute_zv(chunk, precursors_chunks):
@@ -114,10 +108,8 @@ class Distance_Matric(object):
         for pre_chunk in precursors_chunks:
             i = Distance_Matric.vectors_dict["{}{}".format(chunk.getdocID(), chunk.getChunkID())]
             j = Distance_Matric.vectors_dict["{}{}".format(pre_chunk.getdocID(), pre_chunk.getChunkID())]
-            corr = Distance_Matric.corr_matrix[i][j]
-            if (math.isnan(corr)):
-                corr = 0.0
-            sum += corr
+            dist = Distance_Matric.chebyshev_distance[i][j]
+            sum += dist
 
         return sum/int(len(precursors_chunks))
 
@@ -134,5 +126,3 @@ class Distance_Matric(object):
         :return: dictionary where key is chunk's row from distance metric and value is the chunk object
         """
         return self.chunks_index
-
-